924092ce744acbec90e8d3eedf969418efae16c6
Simon Barker <skbarker@me.com>
2020-11-28 12:41:26 +0000

    front end commit

---
+++ react-frontend/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import FeedDisplay from './components/FeedDisplay';
+
+function App() {
+  return <FeedDisplay />;
+}
+
+export default App;
---
+++ react-frontend/src/components/FeedDisplay.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+// const ENDPOINT = 'http://127.0.0.1:5000'; // working express one
+const ENDPOINT = 'http://127.0.0.1:7000'; // not working nestjs one
+
+const FeedDisplay = (props) => {
+
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on('NEW_DATA', data => {
+      console.log('NEW_DATA');
+      setResponse(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <p>Hello World</p>
+      {response}
+    </div>
+  );
+};
+
+export default FeedDisplay;
